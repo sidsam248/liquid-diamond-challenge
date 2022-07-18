95a297ad7a0d302a22293a4d3e7cc08508903ce8
Sid Sam <sidsam248@gmail.com>
2022-07-18 17:12:22 +0530

    fixes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :items do
-    collection { post :import}
+    collection { post :import }
   end
 
   get "/upload", to: "items#upload"
